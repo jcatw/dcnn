fix bug with node classification kernel computation and add several tests

python/util.py
@@ -13,11 +13,11 @@ def A_to_diffusion_kernel(A, k):
     Apow = [np.identity(A.shape[0])]
 
     if k > 0:
-        d = A / A.sum(0)
+        d = A.sum(0)
 
         Apow.append(A / (d + 1.0))
 
-        for i in range(2, k+1):
+        for i in range(2, k + 1):
             Apow.append(np.dot(A / (d + 1.0), Apow[-1]))
 
     return np.transpose(np.asarray(Apow, dtype='float32'), (1, 0, 2))
@@ -25,7 +25,7 @@ def A_to_diffusion_kernel(A, k):
 def A_to_post_sparse_diffusion_kernel(A, k, threshold):
     K = A_to_diffusion_kernel(A, k)
 
-    K[K < threshold] = 0.0
+    K[K <= threshold] = 0.0
 
     return K
 
@@ -33,11 +33,11 @@ def A_to_pre_sparse_diffusion_kernel(A, k, threshold):
     Apow = [np.identity(A.shape[0])]
 
     if k > 0:
-        d = A / A.sum(0)
+        d = A.sum(0)
 
         Apow.append(A / (d + 1.0))
 
-        Apow[-1][Apow[-1] < threshold] = 0.0
+        Apow[-1][Apow[-1] <= threshold] = 0.0
 
         for i in range(2, k + 1):
             Apow.append(np.dot(A / (d + 1.0), Apow[-1]))